30c276919b8f75789bf516e577fd0416414d18e6
Shakil Ali <shakil_419@hotmail.co.uk>
2020-10-07 11:18:48 +0100

    Add files via upload

---
+++ Machine Learning & Data Science Project - Real Estate Price Prediction/server/util.py
@@ -0,0 +1,54 @@
+# IMPORT STATEMENTS
+import json
+import pickle
+import numpy as np
+
+# GLOBAL VARIABLES
+__locations = None
+__data_columns = None
+__model = None
+
+# METHOD FOR ESTIMATING PRICE
+def get_estimated_price(location, sqft, bhk, bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0], 2)
+
+# METHOD FOR RETRIEVING LOCATION NAMES
+def get_location_names():
+    return __locations
+
+# LOADS SAVED ARTIFACTS (COLUMN.JSON)
+def load_saved_artifacts():
+    print("Loading saved artifacts...start")
+    global __data_columns
+    global __locations
+
+    with open("./artifacts/columns.json", 'r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+
+    global __model
+    with open("./artifacts/banglore_home_prices_model.pickle", 'rb') as f:
+        __model = pickle.load(f)
+
+    print("Loading saved artifcats...Complete")
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar',1000,3,3))
+    print(get_estimated_price('1st Phase JP Nagar',1000,2,2))
+    print(get_estimated_price('Kalhalli', 1000,2,2))
+    print(get_estimated_price('Ejipura',1000,2,2))
+
